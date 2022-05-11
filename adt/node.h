f38f4b087856d7329f39987fa37b877e9ac3cebb
marcomoltisanti <moltisanti.m@gmail.com>
2022-05-11 08:38:32 +0200

    Edit node.h

--- adt/node.h
+++ adt/node.h
@@ -1,66 +1,29 @@
-#ifndef LIST_H
-#define LIST_H
+#ifndef LNODE_H
+#define LNODE_H
 
 #include <iostream>
-#include "node.h"
 
 using namespace std;
 
 template <typename T>
-class List {
-	Node<T>* head;
+class Node {
+	T val;
+	Node<T>* next;
+	
+	template<typename U>
+	friend class List;
 	
 	public:
 	
-	List() {
-		head = nullptr;
-		cout << head << endl;
+	Node(T val) : val(val) {
+		next = nullptr;
 	}
 	
-	bool isEmpty() {
-		return head == nullptr;
-	}
+	Node<T>* getNext() const { return this->next; }
 	
-	Node<T> *getHead() const { return head; }
-	
-	void insert(T val) {
-		if(this->isEmpty()) {
-			head = new Node<T>(val);
-			return;
-		}
-	}
-	
-	void insertHead(T val) {	
-		Node<T> *temp = new Node<T>(val);
-		temp->next = head;
-		this->head = temp;
-	}
-	
-	void insertTail(T val) {
-		if(this->isEmpty()) {
-			this->insertHead(val);
-			return;
-		}
-		
-		Node<T> * ptr = head;
-		while(ptr->getNext() != nullptr) {
-			ptr = ptr->getNext();
-		}
-		
-		Node<T> * temp = new Node<T>(val);
-		ptr->next = temp;		
-	}
-	
-	friend ostream& operator<<(ostream& out, const List<T> &list) {
-		out << "List head=" << list.head << endl;
-		Node<T> *ptr = list.head;
-		while(ptr != nullptr) {
-			out << "\t" << *ptr << endl;
-			ptr = ptr->getNext();
-		}
-		
+	friend ostream& operator<<(ostream& out, const Node<T> &node) {
+		out << "node@" << &node << " val=" << node.val << " - next=" << node.next;
 		return out;
-		
 	}
 };
 
